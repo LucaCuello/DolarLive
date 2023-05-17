7e6cadd350f926ab9db8651efe0e388b4de64b1e
Luca <lucagcuello@gmail.com>
2023-05-17 16:08:48 -0300

    Coded getData fetch function

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState<any>(null);
+
+  const URL = "https://api.bluelytics.com.ar/v2/latest";
+
+  useEffect(() => {
+    const getData = async () => {
+      const fetchData = await fetch(URL),
+        json = await fetchData.json(),
+        response = await json;
+
+      setData(response);
+    };
+
+    getData();
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p>Valor venta: ${data?.oficial.value_sell} ARS</p>
+      <p>Valor compra: ${data?.oficial.value_buy} ARS</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
